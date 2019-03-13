106a7bfbe5c9d10daed7889c6cb292e296b13c53
enhanjh <enhanjh@gmail.com>
2019-03-13 19:33:18 +0900

    retrieve portfolio item price

--- query/query_rebalance.py
+++ query/query_rebalance.py
@@ -1,8 +1,8 @@
 
-item_price = "select a.item" \
+pf_item_price = "select a.item" \
              "     , a.tran_day" \
              "     , a.open" \
              "     , a.close" \
              "  from price a" \
-             " where a.item = '{item}'" \
+             " where a.item in (select item from portfolio where mode = '{sttg_num}')" \
              "   and a.tran_day between '{sdate}' and '{edate}';"
